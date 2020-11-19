c17d1a07de2de6ba799aa3d181a2baf78a743d8f
Jakub <jakubcacka@gmail.com>
2020-11-19 22:44:09 +0100

    Bot added to level

--- app/src/main/java/com/shortestwin/game/level/Level.java
+++ app/src/main/java/com/shortestwin/game/level/Level.java
@@ -1,9 +1,12 @@
 package com.shortestwin.game.level;
 
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Rect;
 
 import com.shortestwin.game.GameView;
+import com.shortestwin.game.player.Bot;
 import com.shortestwin.game.player.Player;
 import com.shortestwin.game.level.path.PathFinder;
 
@@ -25,10 +28,12 @@ public class Level {
     private int level;
 
     private Player player;
+    private Bot bot;
 
     public Level(GameView game) {
         this.game = game;
         this.player = game.getPlayer();
+        this.bot = new Bot("Bot", Color.YELLOW);
 
         this.level = 1;
         this.tilesRowCount = 10;
@@ -41,24 +46,16 @@ public class Level {
         this.pathFinder = new PathFinder(this);
         this.levelGenerator = new LevelGenerator(this);
 
-        this.tiles = this.levelGenerator.generateLevel(this.level);
-        this.player.setRect(this.levelGenerator.getStart().getRect());
+        this.generateLevel();
     }
 
     public void draw(Canvas canvas, Paint paint) {
         for(int i = 0; i < tilesRowCount * tilesRowCount; i++) {
             this.tiles[i].draw(canvas, paint);
         }
+
+        this.bot.draw(canvas, paint, this);
         this.player.draw(canvas, paint, this);
-    }
-
-    public void regenerateLevelTiles() {
-        this.level++;
-        this.tilesRowCount += 5;
-        this.rectSize = this.width / this.tilesRowCount;
-
-        this.tiles = this.levelGenerator.generateLevel(this.level);
-        this.player.setRect(this.levelGenerator.getStart().getRect());
     }
 
     public void update() {
@@ -66,7 +63,23 @@ public class Level {
             this.regenerateLevelTiles();
             regenerate = false;
         }
+        this.bot.update();
         this.player.update();
+    }
+
+    public void generateLevel() {
+        this.tiles = this.levelGenerator.generateLevel(this.level);
+        Rect startRect = this.levelGenerator.getStart().getRect();
+        this.bot.setRect(startRect);
+        this.player.setRect(startRect);
+    }
+
+    public void regenerateLevelTiles() {
+        this.level++;
+        this.tilesRowCount += 5;
+        this.rectSize = this.width / this.tilesRowCount;
+
+        this.generateLevel();
     }
 
     public void setRegenerate(boolean regenerate) {
