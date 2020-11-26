1666f38efaf8c8549f913c4b0d110cd7388ab503
Jakub <jakubcacka@gmail.com>
2020-11-26 22:12:22 +0100

    Created ColorBlockView for colors setting

--- app/src/main/java/com/shortestwin/SettingsActivity.java
+++ app/src/main/java/com/shortestwin/SettingsActivity.java
@@ -2,15 +2,7 @@ package com.shortestwin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.DialogInterface;
-import android.graphics.Color;
 import android.os.Bundle;
-import android.widget.Toast;
-
-import com.flask.colorpicker.ColorPickerView;
-import com.flask.colorpicker.OnColorSelectedListener;
-import com.flask.colorpicker.builder.ColorPickerClickListener;
-import com.flask.colorpicker.builder.ColorPickerDialogBuilder;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -18,30 +10,5 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-
-        ColorPickerDialogBuilder
-                .with(this)
-                .setTitle("Choose color")
-                .initialColor(Color.BLACK)
-                .wheelType(ColorPickerView.WHEEL_TYPE.FLOWER)
-                .density(12)
-                .setOnColorSelectedListener(new OnColorSelectedListener() {
-                    @Override
-                    public void onColorSelected(int selectedColor) {
-                    }
-                })
-                .setPositiveButton("ok", new ColorPickerClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int selectedColor, Integer[] allColors) {
-
-                    }
-                })
-                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
-                    }
-                })
-                .build()
-                .show();
     }
 }
---
+++ app/src/main/java/com/shortestwin/game/graphics/ColorBlockView.java
@@ -0,0 +1,81 @@
+package com.shortestwin.game.graphics;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+import android.view.View;
+
+import com.flask.colorpicker.ColorPickerView;
+import com.flask.colorpicker.OnColorSelectedListener;
+import com.flask.colorpicker.builder.ColorPickerClickListener;
+import com.flask.colorpicker.builder.ColorPickerDialogBuilder;
+
+public class ColorBlockView extends View {
+
+    private Context context;
+
+    private Paint paint;
+
+
+    public ColorBlockView(Context context) {
+        super(context);
+        this.init(context);
+    }
+
+    public ColorBlockView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        this.init(context);
+    }
+
+    public ColorBlockView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        this.init(context);
+    }
+
+    private void init(Context context) {
+        this.context = context;
+        this.paint = new Paint();
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(Color.RED);
+        canvas.drawPaint(paint);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        ColorPickerDialogBuilder
+                .with(context)
+                .setTitle("Choose color")
+                .initialColor(Color.BLACK)
+                .wheelType(ColorPickerView.WHEEL_TYPE.FLOWER)
+                .density(12)
+                .setOnColorSelectedListener(new OnColorSelectedListener() {
+                    @Override
+                    public void onColorSelected(int selectedColor) {
+                    }
+                })
+                .setPositiveButton("ok", new ColorPickerClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int selectedColor, Integer[] allColors) {
+
+                    }
+                })
+                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                    }
+                })
+                .build()
+                .show();
+
+        return super.onTouchEvent(event);
+    }
+}
