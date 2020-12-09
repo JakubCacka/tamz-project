99e0cc4b37051189cbe64718706964a9ee03488b
Jakub <jakubcacka@gmail.com>
2020-12-09 20:03:43 +0100

    Reseting the color settings

--- app/src/main/java/com/shortestwin/SettingsActivity.java
+++ app/src/main/java/com/shortestwin/SettingsActivity.java
@@ -2,7 +2,12 @@ package com.shortestwin;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
+
+import com.shortestwin.game.GameView;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -10,5 +15,14 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+    }
+
+    public void resetPreferences(View button) {
+        SharedPreferences prefs = this.getSharedPreferences(GameView.MY_PREFS_NAME, MODE_PRIVATE);
+        prefs.edit().clear().apply();
+        
+        Intent intent = getIntent();
+        finish();
+        startActivity(intent);
     }
 }
